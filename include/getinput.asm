b12cc402716de1946b4f7ae5e306d1a80af8330e
Rexifer <yadel.adelie@gmail.com>
2023-11-07 20:19:32 -0700

    backspace support in getinput

--- include/getinput.asm
+++ include/getinput.asm
@@ -9,6 +9,10 @@ mov ax,0
 int 0x16
 cmp ax,0x1C0D
 je GETINPUTEND
+
+cmp ax,0x0E08
+je GETINPUTBACKSPACE
+
 cmp al,0
 je GETINPUTLOOP
 
@@ -22,6 +26,27 @@ inc bx
 mov ah,0x0e
 int 0x10
 
+jmp GETINPUTLOOP
+
+GETINPUTBACKSPACE:
+push bx
+mov bx,STDIN
+cmp byte[bx],0
+pop bx
+je GETINPUTLOOP
+
+dec bx
+mov byte[bx],0
+int 0x10
+push ax
+mov al,32
+int 0x10
+pop ax
+int 0x10
+push bx
+mov bx,STDIN
+dec byte[bx]
+pop bx
 jmp GETINPUTLOOP
 
 GETINPUTEND:

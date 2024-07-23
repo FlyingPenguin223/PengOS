2456bab8ab1cc6a99c10621dbad3821e68ebe2d0
Rexifer <yadel.adelie@gmail.com>
2024-07-23 10:14:34 -0700

    fix newlines not working at bottom of screen

--- include/newline.asm
+++ include/newline.asm
@@ -4,6 +4,21 @@ mov bx,0
 mov ah,0x03
 int 0x10
 inc dh
+
+cmp dh,25
+jl NEWLINESETCURS
+
+mov ah,0x06
+mov al,1
+mov bh,0b00000111
+mov cx,0
+mov dh,24
+mov dl,79 ; coords of lower right
+int 0x10
+
+mov bh,0
+mov dh,24
+NEWLINESETCURS:
 mov dl,0
 mov ah,0x02
 int 0x10
